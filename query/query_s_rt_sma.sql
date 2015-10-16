5fc2232e04aea4b4032b1abb3af7cf596b0a3395
tedddy <tedddy@foxmail.com>
2015-10-16 10:42:25 +0800

    queries on table s_rt_sma

---
+++ query/query_s_rt_sma.sql
@@ -0,0 +1,7 @@
+SELECT * FROM ying.s_rt_sma ;
+
+SELECT * FROM ying.s_rt_sma order by ids, dt desc;
+
+SELECT count(*) FROM ying.s_rt_sma;
+
+SELECT * FROM ying.s_rt_sma where dt < '2015-10-01' order by dt;
