e758860022751ad9dcf733fc86f5939da72ada88
tedddy <tedddy@foxmail.com>
2015-10-16 10:30:09 +0800

    extend to compute realtime sma for multiPeriods

--- proc/proc_s_rt_get_smaClose_multiPeriods_rt.sql
+++ proc/proc_s_rt_get_smaClose_multiPeriods_rt.sql
@@ -1,14 +1,14 @@
 -- Test the proc
 	SELECT * FROM `ying`.`s_rt` WHERE `ids`='601318' ORDER BY dt DESC;
     
-	CALL `s_rt_get_smaClose_multiPeriods_rt`('2019-09-09 00:00:00', '601318', 5, 10, 20, 30, 60, 120, @out_sma_1, @out_sma_2, @out_sma_3, @out_sma_4, @out_sma_5, @out_sma_6);
+	CALL `s_rt_get_smaClose_rt_multiPeriods_rt`('601318', 0.0, 5, 10, 20, 30, 60, 120, @out_sma_1, @out_sma_2, @out_sma_3, @out_sma_4, @out_sma_5, @out_sma_6);
 	SELECT @out_sma_1, @out_sma_2, @out_sma_3, @out_sma_4, @out_sma_5, @out_sma_6;
 
-DROP PROCEDURE IF EXISTS `s_rt_get_smaClose_multiPeriods_rt`;
+DROP PROCEDURE IF EXISTS `s_rt_get_smaClose_rt_multiPeriods_rt`;
 
 DELIMITER $$
 
-CREATE DEFINER=`gxh`@`%` PROCEDURE `s_rt_get_smaClose_multiPeriods_rt`(
+CREATE DEFINER=`gxh`@`%` PROCEDURE `s_rt_get_smaClose_rt_multiPeriods_rt`(
 
 	IN 	ids VARCHAR(6),
 		in_current_close DECIMAL(6,2), -- variable in_current_close  
@@ -30,27 +30,27 @@ CREATE DEFINER=`gxh`@`%` PROCEDURE `s_rt_get_smaClose_multiPeriods_rt`(
 BEGIN
 
 --  sma_1
-	CALL `s_rt_get_smaClose`(ids, in_current_close, in_smaPeriods_1, @out_sma_1);		
+	CALL `s_rt_get_smaClose_rt`(ids, in_current_close, in_smaPeriods_1, @out_sma_1);		
 	SET out_sma_1 = @out_sma_1;
     
 --  sma_2
-	CALL `s_rt_get_smaClose`(ids, in_current_close, in_smaPeriods_2, @out_sma_2);		
+	CALL `s_rt_get_smaClose_rt`(ids, in_current_close, in_smaPeriods_2, @out_sma_2);		
 	SET out_sma_2 = @out_sma_2;
 
 --  sma_3
-	CALL `s_rt_get_smaClose`(ids, in_current_close, in_smaPeriods_3, @out_sma_3);		
+	CALL `s_rt_get_smaClose_rt`(ids, in_current_close, in_smaPeriods_3, @out_sma_3);		
 	SET out_sma_3 = @out_sma_3;
 
 --  sma_4
-	CALL `s_rt_get_smaClose`(ids, in_current_close, in_smaPeriods_4, @out_sma_4);		
+	CALL `s_rt_get_smaClose_rt`(ids, in_current_close, in_smaPeriods_4, @out_sma_4);		
 	SET out_sma_4 = @out_sma_4;
 
 --  sma_5
-	CALL `s_rt_get_smaClose`(ids, in_current_close, in_smaPeriods_5, @out_sma_5);		
+	CALL `s_rt_get_smaClose_rt`(ids, in_current_close, in_smaPeriods_5, @out_sma_5);		
 	SET out_sma_5 = @out_sma_5;
 
 --  sma_6
-	CALL `s_rt_get_smaClose`(ids, in_current_close, in_smaPeriods_6, @out_sma_6);		
+	CALL `s_rt_get_smaClose_rt`(ids, in_current_close, in_smaPeriods_6, @out_sma_6);		
 	SET out_sma_6 = @out_sma_6;          
 
 END$$
