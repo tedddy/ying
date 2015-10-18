46162151a96616cc6fde9bf816068198c79e2511
tedddy <tedddy@foxmail.com>
2015-10-18 15:24:38 +0800

    add template tables

--- table/table_s_rt.sql
+++ table/table_s_rt.sql
@@ -2,8 +2,7 @@ SELECT * FROM `ying`.`s_rt` order by dt desc, ids;
 SELECT * FROM `ying`.`s_rt` where volume <= 0 and amount <=0 and weibi<=0;
 SELECT * FROM `ying`.`s_rt` where time(dt) = '15:00:00' order by volume;
 
-DELETE FROM `ying`.`s_rt` where close <= 0.0; 
-
+-- DELETE FROM `ying`.`s_rt` where close <= 0.0; 
 -- DELETE FROM `ying`.`s_rt` where dt = '0000-00-00 00:00:00'; 
 -- DELETE FROM `ying`.`hs_s_rt_EM` where TimeSlot = '0000-00-00 00:00:00'; 
 
@@ -23,11 +22,12 @@ CREATE TABLE `ying`.`s_rt` (
   PRIMARY KEY (`ids`,`dt`)
 ) ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
-CREATE TABLE `s_rt` (
+DROP TABLE `tableTMPL`;
+CREATE TABLE `tableTMPL` (
   `ids` varchar(6) NOT NULL COMMENT 'stock id',
-  `close` decimal(6,2) unsigned NOT NULL COMMENT 'close',
-  `volume` int(10) unsigned NOT NULL COMMENT 'volume',
-  `amount` int(10) unsigned NOT NULL COMMENT 'amount',
+  `fieldTMPL` decimal(6,2) unsigned NOT NULL COMMENT 'close',
+  `volume` int(10) unsigned NOT NULL COMMENT '成交量',
+  `amount` int(10) unsigned NOT NULL COMMENT '成交额',
   `chgrate` decimal(5,2) NOT NULL COMMENT '涨跌幅',
   `WeiBi` decimal(6,2) unsigned NOT NULL COMMENT '委比',
   `chgrate5` decimal(5,2) NOT NULL COMMENT '五分钟涨幅',
@@ -36,24 +36,32 @@ CREATE TABLE `s_rt` (
   PRIMARY KEY (`ids`,`dt`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+SELECT * FROM `ying`.`tableTMPL` order by ids desc, dt;
+SELECT count(*) FROM `ying`.`tableTMPL` order by ids desc, dt;
 
-INSERT INTO `s_rt` ( `s_rt`.`ids`,
-    `s_rt`.`close`,
-    `s_rt`.`volume`,
-    `s_rt`.`amount`,
-    `s_rt`.`chgrate`,
-    `s_rt`.`WeiBi`,
-    `s_rt`.`chgrate5`,
-    `s_rt`.`LiangBi`,
-    `s_rt`.`dt`)
-SELECT `hs_s_rt_EM`.`code`,
-    `hs_s_rt_EM`.`close`,
-    `hs_s_rt_EM`.`volume`,
-    `hs_s_rt_EM`.`amount`,
-    `hs_s_rt_EM`.`chgrate`,
-    `hs_s_rt_EM`.`WeiBi`,
-    `hs_s_rt_EM`.`chgrate5`,
-    `hs_s_rt_EM`.`LiangBi`,
-    `hs_s_rt_EM`.`TimeSlot`
-FROM `ying`.`hs_s_rt_EM`;
+-- import data from `s_rt` to `tableTMPL`, where field name `close` is changed to fieldTMPL.
+INSERT INTO `tableTMPL` 
+	( 	
+		`tableTMPL`.`ids`,
+		`tableTMPL`.`fieldTMPL`,
+		`tableTMPL`.`volume`,
+		`tableTMPL`.`amount`,
+		`tableTMPL`.`chgrate`,
+		`tableTMPL`.`WeiBi`,
+		`tableTMPL`.`chgrate5`,
+		`tableTMPL`.`LiangBi`,
+		`tableTMPL`.`dt`
+    )
+SELECT 
+		`s_rt`.`ids`,
+		`s_rt`.`close`,
+		`s_rt`.`volume`,
+		`s_rt`.`amount`,
+		`s_rt`.`chgrate`,
+		`s_rt`.`WeiBi`,
+		`s_rt`.`chgrate5`,
+		`s_rt`.`LiangBi`,
+		`s_rt`.`dt`        
+FROM `ying`.`s_rt`
+WHERE `ids` in ('601318', '000001') order by `dt` desc;
 
