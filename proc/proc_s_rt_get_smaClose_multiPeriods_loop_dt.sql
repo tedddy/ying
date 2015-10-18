ac2f6a97957648611ad6d39dec53ae7ed0a622a9
tedddy <tedddy@foxmail.com>
2015-10-18 15:21:00 +0800

    improve

--- proc/proc_s_rt_get_smaClose_multiPeriods_loop_dt.sql
+++ proc/proc_s_rt_get_smaClose_multiPeriods_loop_dt.sql
@@ -1,11 +1,12 @@
 --  test query in the proc
 	-- DECLARE cursor1 CURSOR FOR 
-    SELECT DISTINCT `dt` FROM `ying`.`s_rt_sma` where dt >= '2005-10-15 13:05:00' and '2015-10-15 13:05:00' ORDER BY `dt` desc;
+    SELECT DISTINCT `dt` FROM `ying`.`s_rt_sma` WHERE dt >= '2005-10-15 13:05:00' AND '2015-10-15 13:05:00' ORDER BY `dt` DESC;
     
-	CALL `s_rt_get_smaClose_multiPeriods_Loop_dt`('2005-10-15 13:05:00', '2015-10-15 13:05:00', '601318', 5, 10, 20, 30, 60, 120);    
-    SELECT * FROM ying.s_rt_sma where ids = '601318' and dt = '2015-10-15 13:05:00' order by dt desc;
+	CALL `s_rt_get_smaClose_multiPeriods_Loop_dt`('2015-10-08 15:05:00', '2015-10-14 01:00:00', '000001', 5, 10, 20, 30, 60, 120); 
     
-	SELECT DISTINCT `dt` FROM `ying`.`s_rt_sma` where dt >= '2015-10-15 13:10:00' and dt <= '2015-10-15 13:10:00' ORDER BY `dt` desc;
+    SELECT * FROM ying.s_rt_sma WHERE ids = '601318' AND dt = '2015-10-15 13:05:00' ORDER BY dt DESC;
+    
+	SELECT DISTINCT `dt` FROM `ying`.`s_rt_sma` WHERE dt >= '2015-09-08 15:05:00' AND dt <= '2015-10-15 13:10:00' ORDER BY `dt` DESC;
     
 	CALL `s_rt_get_smaClose_multiPeriods`('2015-10-15 13:10:00', '601318', 5, 10, 20, 30, 60, 120, @out_sma_1, @out_sma_2, @out_sma_3, @out_sma_4, @out_sma_5, @out_sma_6);
 	SELECT @out_sma_1, @out_sma_2, @out_sma_3, @out_sma_4, @out_sma_5, @out_sma_6;
@@ -30,36 +31,31 @@ BEGIN
 
 -- DECLARE cursor_fetch_tmps      
 	DECLARE cursor_fetch_tmp DATETIME;   -- for dt 
-    
-	DECLARE loop_cnt SMALLINT(3); -- variable for loop counter     
 
 	DECLARE record_fetch_end TINYINT DEFAULT 0; -- DECLARE variable for error handler
         
-	DECLARE cursor1 CURSOR FOR SELECT DISTINCT `dt` FROM `ying`.`s_rt` where dt >= in_dt_low and dt <= in_dt_high ORDER BY `dt` desc; -- DECLARE coursors  
+	DECLARE cursor1 CURSOR FOR SELECT DISTINCT `dt` FROM `ying`.`s_rt` WHERE dt >= in_dt_low AND dt <= in_dt_high ORDER BY `dt` DESC; -- DECLARE coursors  
 
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET record_fetch_end = 1; -- DECLARE error handler for "NOT FOUND"	
     
-	SET loop_cnt = 0;  
 -- open cursor
 	OPEN cursor1;
 	-- Loop	of cursor  
-		cursor_LOOP: LOOP
+		CURSOR_LOOP: LOOP
 		
 			FETCH cursor1 INTO cursor_fetch_tmp; -- fetch result row into cursor_fetch_tmp tedd
-			
-			SET loop_cnt = loop_cnt + 1; -- increment the loop counter
-            
+            		
 		-- actions	
 			CALL `s_rt_get_smaClose_multiPeriods`(cursor_fetch_tmp,in_ids,5,10,20,30,60,120, @out_sma_1,@out_sma_2,@out_sma_3,@out_sma_4,@out_sma_5,@out_sma_6);
 			
-			insert into `ying`.`s_rt_sma` (`dt`,`ids`,`sma5c`,`sma10c`,`sma20c`,`sma30c`,`sma60c`,`sma120c`) values (cursor_fetch_tmp,in_ids,@out_sma_1, @out_sma_2, @out_sma_3, @out_sma_4, @out_sma_5, @out_sma_6) ON DUPLICATE KEY UPDATE `sma5c` =  @out_sma_1, `sma10c` =  @out_sma_2, `sma20c` =  @out_sma_3, `sma30c` =  @out_sma_4, `sma60c` =  @out_sma_5, `sma120c` =  @out_sma_6;
-			
+			INSERT INTO `ying`.`s_rt_sma` (`dt`,`ids`,`sma5c`,`sma10c`,`sma20c`,`sma30c`,`sma60c`,`sma120c`) VALUES (cursor_fetch_tmp,in_ids,@out_sma_1, @out_sma_2, @out_sma_3, @out_sma_4, @out_sma_5, @out_sma_6) ON DUPLICATE KEY UPDATE `sma5c` =  @out_sma_1, `sma10c` =  @out_sma_2, `sma20c` =  @out_sma_3, `sma30c` =  @out_sma_4, `sma60c` =  @out_sma_5, `sma120c` =  @out_sma_6;
+
 		-- break from loop if reach the end of the cursor
 			IF record_fetch_end THEN
-				LEAVE cursor_LOOP;
+				LEAVE CURSOR_LOOP;
 			END IF;		
-
-		END LOOP cursor_LOOP;   
+	
+		END LOOP CURSOR_LOOP;   
     
 	CLOSE cursor1;		
     
