2beba01d09761b415ba5aa75dcc690c3eb062d9e
tedddy <tedddy@foxmail.com>
2015-10-21 10:57:39 +0800

    Migrate from table `hs_s_xts` to table `s_xts`: step 2
    change table name
        delete id, time, fR
        change code to ids, date
    to d
        change primary key and delete unique key, delete AUTO_INCREMENT

--- table/table_s_xts.sql
+++ table/table_s_xts.sql
@@ -1,16 +1,23 @@
-CREATE TABLE `hs_s_xts` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `code` varchar(6) NOT NULL COMMENT '代码id; code id in table hk_s and in table hk_s',
-  `date` date DEFAULT NULL COMMENT '交易日期; trade date',
-  `open` decimal(7,3) unsigned DEFAULT NULL COMMENT '开盘价',
-  `high` decimal(7,3) unsigned DEFAULT NULL COMMENT '最高价',
-  `low` decimal(7,3) unsigned DEFAULT NULL COMMENT '最低价',
-  `close` decimal(7,3) unsigned DEFAULT NULL COMMENT '收盘价',
-  `volume` int(11) unsigned DEFAULT NULL COMMENT '成交量',
-  `amount` bigint(12) unsigned DEFAULT NULL COMMENT '股票成交额',
-  `time` time DEFAULT NULL,
-  `adjusted` decimal(7,3) unsigned DEFAULT NULL COMMENT '收盘价',
-  `fR` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'Result of data collection\n0: fail; 1: success',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `stock_date` (`code`,`date`)
-) ENGINE=InnoDB AUTO_INCREMENT=141390 DEFAULT CHARSET=utf8;
+-- Task: Migrate from table `hs_s_xts` to table `s_xts`
+
+/* Migrate from table `hs_s_xts` to table `s_xts`: step 2 
+	10:16 2015-10-21 
+    change table name
+	delete id, time, fR
+	change code to ids, date to d
+    change primary key and delete unique key, delete AUTO_INCREMENT */
+
+-- 10:15 2015-10-21
+
+CREATE TABLE `s_xts` (
+  `ids` varchar(6) NOT NULL COMMENT 'stock id 股票代码',
+  `d` date DEFAULT NULL COMMENT 'trade date 交易日期',
+  `open` decimal(7,3) unsigned DEFAULT NULL COMMENT 'open 开盘价',
+  `high` decimal(7,3) unsigned DEFAULT NULL COMMENT 'high 最高价',
+  `low` decimal(7,3) unsigned DEFAULT NULL COMMENT 'low 最低价',
+  `close` decimal(7,3) unsigned DEFAULT NULL COMMENT 'close 收盘价',
+  `volume` int(11) unsigned DEFAULT NULL COMMENT 'volume 成交量',
+  `amount` bigint(12) unsigned DEFAULT NULL COMMENT 'amount 股票成交额',
+  `adjusted` decimal(7,3) unsigned DEFAULT NULL COMMENT 'adjusted close 复权后的收盘价',
+  PRIMARY KEY (`ids`,`d`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
