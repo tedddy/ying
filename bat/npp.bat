a91674ab4b03a4218200cc5a7691ec932f0ed910
tedddy <tedddy@foxmail.com>
2015-10-25 08:27:08 +0800

    configure default edior for "open file" in github. unsuccessful

---
+++ bat/npp.bat
@@ -0,0 +1,4 @@
+# git config --global core.editor E://bYun/securities/ying/bat/npp.bat
+# refer to How can I set up an editor to work with Git on Windows? - Stack Overflow
+#!/bin/sh
+"D:/Program Files (x86)/Notepad++/notepad++.exe" -multiInst -notabbar -nosession -noPlugin "$*"
