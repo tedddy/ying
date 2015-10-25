d11c4824857fe2d2ac29b38e95192e9b32ddd00b
tedddy <tedddy@foxmail.com>
2015-10-25 22:31:33 +0800

    l

--- locoy/s_xts_adj_hst_locoy.sql
+++ locoy/s_xts_adj_hst_locoy.sql
@@ -1,24 +1,28 @@
 -- url_s_xts_adj_hst_sina_2015, used in Project hs_s_xts_histroy_sina_2015
-SELECT DISTINCT CONCAT('http://vip.stock.finance.sina.com.cn/corp/go.php/vMS_FuQuanMarketHistory/stockid/', s.code, '.phtml?year=', date.year,'&jidu=', date.quarter) AS 'url_s_xts_adj_hst_sina_2015' FROM hs_s as s join year_quarter as date  where s.fW > 0 AND year = 2015 and concat(year(s.pDate), quarter(s.pDate)) <= concat(date.year, date.quarter);
+SELECT DISTINCT CONCAT('http://vip.stock.finance.sina.com.cn/corp/go.php/vMS_FuQuanMarketHistory/stockid/', s.code, '.phtml?year=', date.year,'&jidu=', date.quarter) AS 'url_s_xts_adj_hst_sina_2015' FROM hs_s AS s JOIN year_quarter AS date  WHERE s.fW > 0 AND year = 2015 AND CONCAT(YEAR(s.pDate), QUARTER(s.pDate)) <= CONCAT(date.year, date.quarter);
+
+-- url_s_xts_adj_hst_sina_missing, used in Project hs_s_xts_histroy_sina_missing
+SELECT DISTINCT CONCAT('http://vip.stock.finance.sina.com.cn/corp/go.php/vMS_FuQuanMarketHistory/stockid/', s.code, '.phtml?year=', date.year,'&jidu=', date.quarter) AS 'url_s_xts_adj_hst_sina_missing_151025' FROM hs_s AS s JOIN year_quarter AS date  WHERE s.fW > 0 AND s.code IN (SELECT DISTINCT ids FROM `ying`.`s_xts_adj_hst_sina` WHERE high > 9999 ORDER BY `ids`, `d` DESC) AND year IN (2013,2014,2015) AND CONCAT(YEAR(s.pDate), QUARTER(s.pDate)) <= CONCAT(date.year, date.quarter);
 
 SELECT * FROM `ying`.`s_xts_adj_hst_sina`;		
 -- DROP TABLE `ying`.`s_xts_adj_hst_sina`;
 CREATE TABLE `s_xts_adj_hst_sina` (
-  `ids` VARCHAR(6) NOT NULL COMMENT 'stock id 股票代码',
-  `d` DATE DEFAULT NULL COMMENT 'trade date 交易日期',
-  `open` DECIMAL(6,2) UNSIGNED NOT NULL COMMENT 'open 开盘价',
-  `high` DECIMAL(6,2) UNSIGNED NOT NULL COMMENT 'high 最高价',
-  `low` DECIMAL(6,2) UNSIGNED NOT NULL COMMENT 'low 最低价',
-  `close` DECIMAL(6,2) UNSIGNED NOT NULL COMMENT 'close 收盘价',
-  `volume` INT(11) UNSIGNED NOT NULL COMMENT 'volume 成交量',
-  `amount` BIGINT(12) UNSIGNED NOT NULL COMMENT 'amount 股票成交额',
-  `fa` DECIMAL(7,3) UNSIGNED NOT NULL DEFAULT '0.00' COMMENT 'fa 复权因子 (factor for adjusted close)',
+  `ids` varchar(6) NOT NULL COMMENT 'stock id 股票代码',
+  `d` date NOT NULL DEFAULT '0000-00-00' COMMENT 'trade date 交易日期',
+  `open` decimal(10,3) unsigned NOT NULL COMMENT 'open 开盘价',
+  `high` decimal(10,3) unsigned NOT NULL COMMENT 'high 最高价',
+  `low` decimal(10,3) unsigned NOT NULL COMMENT 'low 最低价',
+  `close` decimal(10,3) unsigned NOT NULL COMMENT 'close 收盘价',
+  `volume` int(11) unsigned NOT NULL COMMENT 'volume 成交量',
+  `amount` bigint(12) unsigned NOT NULL COMMENT 'amount 股票成交额',
+  `fa` decimal(10,3) unsigned NOT NULL DEFAULT '0.000' COMMENT 'fa 复权因子 (factor for adjusted close)',
   PRIMARY KEY (`ids`,`d`)
-) ENGINE=INNODB DEFAULT CHARSET=UTF8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 -- DROP PROCEDURE `ying`.`s_xts_adj_hst_sina`;
 DELIMITER $$
-CREATE DEFINER=`gxh`@`%` PROCEDURE `s_xts_adj_hst_sina`(IN ids VARCHAR(6), d DATE, open DECIMAL(7,3) UNSIGNED, high DECIMAL(7,3) UNSIGNED, low DECIMAL(7,3) UNSIGNED, close DECIMAL(7,3) UNSIGNED, volume DECIMAL(15,3) UNSIGNED, amount DECIMAL(16,3) UNSIGNED, fa DECIMAL(7,3) UNSIGNED)
+CREATE DEFINER=`gxh`@`%` PROCEDURE `s_xts_adj_hst_sina`(IN ids VARCHAR(6), d DATE, open DECIMAL(10,3) UNSIGNED, high DECIMAL(10,3) UNSIGNED, low DECIMAL(10,3) UNSIGNED, close DECIMAL(10,3) UNSIGNED, volume DECIMAL(15,3) UNSIGNED, amount DECIMAL(16,3) UNSIGNED, fa DECIMAL(10,3) UNSIGNED)
 
 BEGIN
 	SET `ids` = IF(ids = '', NULL, ids);
@@ -27,9 +31,10 @@ BEGIN
 	SET `high` = IF(high = '', NULL, high);
 	SET `low` = IF(low = '', NULL, low);
 	SET `close` = IF(close = '', NULL, close);
-	SET `volume` = IF(volume = '', NULL, round(volume * 0.01)); -- 单位：手
-	SET `amount` = IF(amount = '', NULL, round(amount * 0.0001)); -- 单位：万元
+	SET `volume` = IF(volume = '', NULL, ROUND(volume * 0.01)); -- 单位：手
+	SET `amount` = IF(amount = '', NULL, ROUND(amount * 0.0001)); -- 单位：万元
 	SET `fa` = IF(fa = '', NULL, fa);
+    -- SET `fa` = IF(fa = '', NULL, CASE WHEN fa > 4000 THEN ROUND(fa) WHEN fa > 1000 THEN ROUND(fa, 1) WHEN fa > 100 THEN ROUND(fa, 2) ELSE fa END);
 
 	IF volume > 0 AND volume IS NOT NULL THEN
 		INSERT INTO `s_xts_adj_hst_sina` (`ids`, `d`, `open`, `high`, `low`, `close`, `volume`, `amount`, `fa`) VALUES (ids, d, open, high, low, close, volume, amount, fa) 
