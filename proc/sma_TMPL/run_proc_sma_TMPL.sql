6bd5f3937937c224b24ce836dc255ce796ea198a
tedddy <tedddy@foxmail.com>
2015-10-26 23:59:43 +0800

    q&r

--- proc/sma_TMPL/run_proc_sma_TMPL.sql
+++ proc/sma_TMPL/run_proc_sma_TMPL.sql
@@ -1,10 +1,6 @@
-SELECT * FROM s_xts_adj;
-
-SELECT count(*) FROM s_xts_adj;
+-- mysql ying_calc -u gxh -p;
 
 CALL `s_xts_adj_ids_d_close_sma_multiPeriods_loop_dt_ids`('2015-01-01 00:00:01', '2016-01-01 00:00:01', 5, 10, 20, 30, 60, 120); -- run this in putty console
-
--- mysql ying_calc -u gxh -p;
 
 CALL `s_xts_adj_ids_d_amount_sma_multiPeriods_loop_dt_ids`('2015-01-01 00:00:01', '2016-01-01 00:00:01', 5, 10, 20, 30, 60, 120); -- run this in putty console
 
