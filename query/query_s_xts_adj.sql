ea6d047ea9a61f72ade77022a9cd8e760a6b8ab7
tedddy <tedddy@foxmail.com>
2015-10-27 00:00:26 +0800

    i

--- query/query_s_xts_adj.sql
+++ query/query_s_xts_adj.sql
@@ -2,12 +2,3 @@ SELECT * FROM s_xts_adj;
 
 SELECT count(*) FROM s_xts_adj;
 
-CALL `s_xts_adj_ids_d_close_sma_multiPeriods_loop_dt_ids`('2015-01-01 00:00:01', '2016-01-01 00:00:01', 5, 10, 20, 30, 60, 120); -- run this in putty console
-
--- mysql ying_calc -u gxh -p;
-
-CALL `s_xts_adj_ids_d_amount_sma_multiPeriods_loop_dt_ids`('2015-01-01 00:00:01', '2016-01-01 00:00:01', 5, 10, 20, 30, 60, 120); -- run this in putty console
-
-CALL `s_xts_adj_ids_d_close_sma_multiPeriods_loop_dt_ids`('2014-01-01 00:00:01', '2015-01-01 00:00:01', 5, 10, 20, 30, 60, 120);
-
-CALL `s_xts_adj_ids_d_amount_sma_multiPeriods_loop_dt_ids`('2014-01-01 00:00:01', '2015-01-01 00:00:01', 5, 10, 20, 30, 60, 120);
