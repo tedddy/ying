dcabab0b157d33e2b3469f2aa749cc698099b4b0
tedddy <tedddy@foxmail.com>
2015-10-26 23:56:45 +0800

    i: uppercase

--- proc/sma_TMPL/proc_s_xts_adj_ids_d_amount_sma.sql
+++ proc/sma_TMPL/proc_s_xts_adj_ids_d_amount_sma.sql
@@ -1,20 +1,20 @@
 /* 	
-	replace `s_xts_adj` with `s_xts_adj`, `ids` with `ids`, `d` with `d`, `close` with `close`
-	save file proc_s_xts_adj_ids_d_close_sma.sql as another file accordingly
+	replace `s_xts_adj` with `s_xts_adj`, `ids` with `ids`, `d` with `d`, `amount` with `amount`
+	save file proc_s_xts_adj_ids_d_amount_sma.sql as another file accordingly
 	replace
 		s_xts_adj	with		37 replaced
 		ids		with			 replaced
 		d		with			49 replaced
-		close	with			49 replaced
-	execute file proc_s_xts_adj_ids_d_close_sma.sql & test all procedures
+		amount	with			49 replaced
+	execute file proc_s_xts_adj_ids_d_amount_sma.sql & test all procedures
 */
--- file name: E:\user_tony\Documents\GitHub\ying\proc\sma_TMPL\proc_s_xts_adj_ids_d_close_sma.sql
+-- file name: E:\user_tony\Documents\GitHub\ying\proc\sma_TMPL\proc_s_xts_adj_ids_d_amount_sma.sql
 
--- instructions for PROCEDURE `s_xts_adj_ids_d_close_sma`
+-- instructions for PROCEDURE `s_xts_adj_ids_d_amount_sma`
 	-- replace `d` and ids with acorrding fields names
 		-- NOTE: remember to change data types accordingly.
 	-- replace s_xts_adj with table name;
-    -- replace close wiht the field name of the sma calc;
+    -- replace amount wiht the field name of the sma calc;
     -- replace the date type of out_sma, cursor_fetch_tmp, and sum with the according data types;
 		-- NOTE: out_sma and cursor_fetch_tmp are usually the same data type.
         -- NOTE: sum is better to be big, especially when loop_cnt is large.
@@ -22,7 +22,7 @@
 
 		
 -- Test the proc
--- 	CALL `s_xts_adj_ids_d_close_sma`('2019-09-09', '601318', 10, @out_sma);
+-- 	CALL `s_xts_adj_ids_d_amount_sma`('2019-09-09', '601318', 10, @out_sma);
 -- 	SELECT @out_sma;    
 
 -- DROP TABLE IF EXISTS `s_xts_adj_sma`;
@@ -34,17 +34,17 @@ CREATE TABLE IF NOT EXISTS `s_xts_adj_sma` (
 ) ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
 ALTER TABLE `s_xts_adj_sma`
-ADD COLUMN `close5` DECIMAL(6,2) UNSIGNED DEFAULT NULL COMMENT 'close 5 periods均线',
-ADD COLUMN `close10` DECIMAL(6,2) UNSIGNED DEFAULT NULL COMMENT 'close 10 periods均线',
-ADD COLUMN  `close20` DECIMAL(6,2) UNSIGNED DEFAULT NULL COMMENT 'close 20 periods均线',
-ADD COLUMN  `close30` DECIMAL(6,2) UNSIGNED DEFAULT NULL COMMENT 'close 30 periods均线',
-ADD COLUMN  `close60` DECIMAL(6,2) UNSIGNED DEFAULT NULL COMMENT 'close 60 periods均线',
-ADD COLUMN  `close120` DECIMAL(6,2) UNSIGNED DEFAULT NULL COMMENT 'close 120 periods均线';
+ADD COLUMN `amount5` DECIMAL(6,2) UNSIGNED DEFAULT NULL COMMENT 'amount 5 periods均线',
+ADD COLUMN `amount10` DECIMAL(6,2) UNSIGNED DEFAULT NULL COMMENT 'amount 10 periods均线',
+ADD COLUMN  `amount20` DECIMAL(6,2) UNSIGNED DEFAULT NULL COMMENT 'amount 20 periods均线',
+ADD COLUMN  `amount30` DECIMAL(6,2) UNSIGNED DEFAULT NULL COMMENT 'amount 30 periods均线',
+ADD COLUMN  `amount60` DECIMAL(6,2) UNSIGNED DEFAULT NULL COMMENT 'amount 60 periods均线',
+ADD COLUMN  `amount120` DECIMAL(6,2) UNSIGNED DEFAULT NULL COMMENT 'amount 120 periods均线';
 
-DROP PROCEDURE IF EXISTS `s_xts_adj_ids_d_close_sma`;
+DROP PROCEDURE IF EXISTS `s_xts_adj_ids_d_amount_sma`;
 -- This procedure compute sma (simple moving average) for stock (ids) at given datetime (`d`).
 DELIMITER $$ 
-CREATE PROCEDURE `s_xts_adj_ids_d_close_sma`(
+CREATE PROCEDURE `s_xts_adj_ids_d_amount_sma`(
 	IN 	
 		in_d DATETIME, -- variable: datetime. If the value of this variable is large (such as '2019-09-09'), then the sma we get from this proc is for the latest datetime in table s_xts_adj. 
 		in_ids VARCHAR(25), -- variable: stock id
@@ -65,7 +65,7 @@ BEGIN
    
 -- 	DECLARE coursors 
 		DECLARE cursor1 CURSOR FOR -- variable for the first cursor. 
-			SELECT `close` FROM `s_xts_adj` WHERE `ids` = in_ids AND `d` <= in_d ORDER BY `d` DESC LIMIT LIMIT_NUMBER_FOR_CURSOR;
+			SELECT `amount` FROM `s_xts_adj` WHERE `ids` = in_ids AND `d` <= in_d ORDER BY `d` DESC LIMIT LIMIT_NUMBER_FOR_CURSOR;
 
 -- 	DECLARE error handler for "NOT FOUND"
 		DECLARE CONTINUE HANDLER FOR NOT FOUND SET record_fetch_end = 1;
@@ -110,13 +110,13 @@ DELIMITER ;
 -- Test the proc
 -- 	SELECT * FROM `s_xts_adj` WHERE `ids`='601318' ORDER BY d DESC;
 --     
--- 	CALL `s_xts_adj_ids_d_close_sma_multiPeriods`('2019-09-09 00:00:00', '601318', 5, 10, 20, 30, 60, 120, @out_sma_1, @out_sma_2, @out_sma_3, @out_sma_4, @out_sma_5, @out_sma_6);
+-- 	CALL `s_xts_adj_ids_d_amount_sma_multiPeriods`('2019-09-09 00:00:00', '601318', 5, 10, 20, 30, 60, 120, @out_sma_1, @out_sma_2, @out_sma_3, @out_sma_4, @out_sma_5, @out_sma_6);
 -- 	SELECT @out_sma_1, @out_sma_2, @out_sma_3, @out_sma_4, @out_sma_5, @out_sma_6;
 
-DROP PROCEDURE IF EXISTS `s_xts_adj_ids_d_close_sma_multiPeriods`;
+DROP PROCEDURE IF EXISTS `s_xts_adj_ids_d_amount_sma_multiPeriods`;
 
 DELIMITER $$
-CREATE DEFINER=`gxh`@`%` PROCEDURE `s_xts_adj_ids_d_close_sma_multiPeriods`
+CREATE DEFINER=`gxh`@`%` PROCEDURE `s_xts_adj_ids_d_amount_sma_multiPeriods`
 	(
 	IN 	
 		in_d DATETIME, -- variable datetime. If the value of this variable is large (such as '2019-09-09'), then the sma we get from this proc is for the latest datetime in table s_xts_adj.  
@@ -139,44 +139,44 @@ CREATE DEFINER=`gxh`@`%` PROCEDURE `s_xts_adj_ids_d_close_sma_multiPeriods`
 BEGIN
 
 --  sma_1
-	CALL `s_xts_adj_ids_d_close_sma`(in_d, in_ids, in_smaPeriods_1, @out_sma_1);		
+	CALL `s_xts_adj_ids_d_amount_sma`(in_d, in_ids, in_smaPeriods_1, @out_sma_1);		
 	SET out_sma_1 = @out_sma_1;
     
 --  sma_2
-	CALL `s_xts_adj_ids_d_close_sma`(in_d, in_ids, in_smaPeriods_2, @out_sma_2);		
+	CALL `s_xts_adj_ids_d_amount_sma`(in_d, in_ids, in_smaPeriods_2, @out_sma_2);		
 	SET out_sma_2 = @out_sma_2;
 
 --  sma_3
-	CALL `s_xts_adj_ids_d_close_sma`(in_d, in_ids, in_smaPeriods_3, @out_sma_3);		
+	CALL `s_xts_adj_ids_d_amount_sma`(in_d, in_ids, in_smaPeriods_3, @out_sma_3);		
 	SET out_sma_3 = @out_sma_3;
 
 --  sma_4
-	CALL `s_xts_adj_ids_d_close_sma`(in_d, in_ids, in_smaPeriods_4, @out_sma_4);		
+	CALL `s_xts_adj_ids_d_amount_sma`(in_d, in_ids, in_smaPeriods_4, @out_sma_4);		
 	SET out_sma_4 = @out_sma_4;
 
 --  sma_5
-	CALL `s_xts_adj_ids_d_close_sma`(in_d, in_ids, in_smaPeriods_5, @out_sma_5);		
+	CALL `s_xts_adj_ids_d_amount_sma`(in_d, in_ids, in_smaPeriods_5, @out_sma_5);		
 	SET out_sma_5 = @out_sma_5;
 
 --  sma_6
-	CALL `s_xts_adj_ids_d_close_sma`(in_d, in_ids, in_smaPeriods_6, @out_sma_6);		
+	CALL `s_xts_adj_ids_d_amount_sma`(in_d, in_ids, in_smaPeriods_6, @out_sma_6);		
 	SET out_sma_6 = @out_sma_6;          
 
 END$$
 DELIMITER ;
 
 --  test query in the proc    
--- 	CALL `s_xts_adj_ids_d_close_sma_multiPeriods_loop_dt_ids`('2005-10-13 13:05:00', '2019-10-15 15:05:00', 5, 10, 20, 30, 60, 120);     
+-- 	CALL `s_xts_adj_ids_d_amount_sma_multiPeriods_loop_dt_ids`('2005-10-13 13:05:00', '2019-10-15 15:05:00', 5, 10, 20, 30, 60, 120);     
 -- 	SELECT * FROM `s_xts_adj_sma` WHERE `d` >= '2005-10-13 13:05:00' AND `d` <= '2019-10-15 15:05:00' ORDER BY `d` DESC;
 -- 	select `d` FROM `s_xts_adj`;
--- instructions for PROCEDURE `s_xts_adj_ids_d_close_sma_multiPeriods_loop_dt_ids`
+-- instructions for PROCEDURE `s_xts_adj_ids_d_amount_sma_multiPeriods_loop_dt_ids`
 	-- create table `s_xts_adj_sma` according to E:\bYun\securities\ying\table\table_s_xts_adj_sma.sql
 	-- replace fields names accordingly, to which sma's are inserted 
     
-DROP PROCEDURE IF EXISTS `s_xts_adj_ids_d_close_sma_multiPeriods_loop_dt_ids`; 
+DROP PROCEDURE IF EXISTS `s_xts_adj_ids_d_amount_sma_multiPeriods_loop_dt_ids`; 
 -- This proc loops through two fields to compute sma's
 DELIMITER $$
-CREATE DEFINER=`gxh`@`%` PROCEDURE `s_xts_adj_ids_d_close_sma_multiPeriods_loop_dt_ids`(
+CREATE DEFINER=`gxh`@`%` PROCEDURE `s_xts_adj_ids_d_amount_sma_multiPeriods_loop_dt_ids`(
 
 	IN 	in_dt_low DATETIME, -- variable for the lowest datetime in the selection of the cursor. 
         in_dt_high DATETIME, -- variable for the highest datetime in the selection of the cursor.ids VARCHAR(6),
@@ -208,9 +208,9 @@ BEGIN
 			FETCH cursor1 INTO cursor_fetch_tmp_dt, cursor_fetch_tmp_ids; -- fetch result row into cursor_fetch_tmp tedd
             		
 		-- 	actions	
-			CALL `s_xts_adj_ids_d_close_sma_multiPeriods`(cursor_fetch_tmp_dt, cursor_fetch_tmp_ids,5,10,20,30,60,120, @out_sma_1,@out_sma_2,@out_sma_3,@out_sma_4,@out_sma_5,@out_sma_6);
+			CALL `s_xts_adj_ids_d_amount_sma_multiPeriods`(cursor_fetch_tmp_dt, cursor_fetch_tmp_ids,5,10,20,30,60,120, @out_sma_1,@out_sma_2,@out_sma_3,@out_sma_4,@out_sma_5,@out_sma_6);
 			
-			INSERT INTO `s_xts_adj_sma` (`d`,`ids`,`close5`,`close10`,`close20`,`close30`,`close60`,`close120`) VALUES (cursor_fetch_tmp_dt, cursor_fetch_tmp_ids, @out_sma_1, @out_sma_2, @out_sma_3, @out_sma_4, @out_sma_5, @out_sma_6) ON DUPLICATE KEY UPDATE `close5` =  @out_sma_1, `close10` =  @out_sma_2, `close20` =  @out_sma_3, `close30` =  @out_sma_4, `close60` =  @out_sma_5, `close120` =  @out_sma_6;
+			INSERT INTO `s_xts_adj_sma` (`d`,`ids`,`amount5`,`amount10`,`amount20`,`amount30`,`amount60`,`amount120`) VALUES (cursor_fetch_tmp_dt, cursor_fetch_tmp_ids, @out_sma_1, @out_sma_2, @out_sma_3, @out_sma_4, @out_sma_5, @out_sma_6) ON DUPLICATE KEY UPDATE `amount5` =  @out_sma_1, `amount10` =  @out_sma_2, `amount20` =  @out_sma_3, `amount30` =  @out_sma_4, `amount60` =  @out_sma_5, `amount120` =  @out_sma_6;
 
 		-- 	break from loop if reach the end of the cursor
 			IF record_fetch_end THEN
