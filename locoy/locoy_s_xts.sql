8ae3ffe74c047f19704a0c5e8063fbd6edaa2da7
tedddy <tedddy@foxmail.com>
2015-10-31 21:06:47 +0800

    delete clo field; insert records also into table `ying_calc`.`s_xts_adj`

--- locoy/locoy_s_xts.sql
+++ locoy/locoy_s_xts.sql
@@ -12,56 +12,22 @@
 -- DROP TABLE `s_xts`;
 
 CREATE TABLE `s_xts` (
-  `ids` VARCHAR(6) NOT NULL COMMENT 'stock id 股票代码',
-  `d` DATE DEFAULT NULL COMMENT 'trade date 交易日期',
-  `open` DECIMAL(6,2) UNSIGNED NOT NULL COMMENT 'open 开盘价',
-  `high` DECIMAL(6,2) UNSIGNED NOT NULL COMMENT 'high 最高价',
-  `low` DECIMAL(6,2) UNSIGNED NOT NULL COMMENT 'low 最低价',
-  `close` DECIMAL(6,2) UNSIGNED NOT NULL COMMENT 'close 收盘价',
-  `volume` INT(11) UNSIGNED NOT NULL COMMENT 'volume 成交量',
-  `amount` BIGINT(12) UNSIGNED NOT NULL COMMENT 'amount 股票成交额',
-  `clo` DECIMAL(6,2) UNSIGNED NOT NULL DEFAULT '0.00' COMMENT 'adjusted close 复权后的收盘价',
+  `ids` varchar(6) NOT NULL COMMENT 'stock id 股票代码',
+  `d` date NOT NULL DEFAULT '0000-00-00' COMMENT 'trade date 交易日期',
+  `open` decimal(6,2) unsigned NOT NULL COMMENT 'open 开盘价',
+  `high` decimal(6,2) unsigned NOT NULL COMMENT 'high 最高价',
+  `low` decimal(6,2) unsigned NOT NULL COMMENT 'low 最低价',
+  `close` decimal(6,2) unsigned NOT NULL COMMENT 'close 收盘价',
+  `volume` int(11) unsigned NOT NULL COMMENT 'volume 成交量',
+  `amount` bigint(12) unsigned NOT NULL COMMENT 'amount 股票成交额',
   PRIMARY KEY (`ids`,`d`)
-) ENGINE=INNODB DEFAULT CHARSET=UTF8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 /* Migrate locoy project `hs_s_xts` to `s_xts` step 1: migrate table 
 	10:58 2015-10-21 
     5. import date from table `hs_s_xts` to table `s_xts` */
-
-INSERT INTO `s_xts`
-	(`ids`,
-	`d`,
-	`open`,
-	`high`,
-	`low`,
-	`close`,
-	`volume`,
-	`amount`,
-	`clo`)
-SELECT 
-    `hs_s_xts`.`code`,
-    `hs_s_xts`.`date`,
-    `hs_s_xts`.`open`,
-    `hs_s_xts`.`high`,
-    `hs_s_xts`.`low`,
-    `hs_s_xts`.`close`,
-    `hs_s_xts`.`volume`,
-    `hs_s_xts`.`amount`,
-    `hs_s_xts`.`clo`
-FROM `hs_s_xts` WHERE date = '2015-10-21' ;
--- 
-SELECT COUNT(*) FROM `hs_s_xts`;
-SELECT COUNT(*) FROM `s_xts`;
-SELECT * FROM `hs_s_xts_fast` ORDER BY date DESC;
-SELECT * FROM `s_xts` WHERE d = '2015-10-22' ORDER BY d DESC;
-
--- clean data
-SELECT COUNT(*) FROM `s_xts` WHERE `amount` = 0 OR volume = 0;
-SELECT * FROM `s_xts` WHERE `amount` = 0 OR volume = 0;
-DELETE FROM `s_xts` WHERE `amount` = 0 OR volume = 0;
-
--- update field clo 
-UPDATE `s_xts` SET `clo` = close;
+    
 
 /* Migrate locoy project `hs_s_xts` to `s_xts` step 2: migrate procedure
 	10:16 2015-10-21 
@@ -75,7 +41,6 @@ DELIMITER $$
 CREATE DEFINER=`gxh`@`%` PROCEDURE `s_xts`(IN ids VARCHAR(6), d DATE, open DECIMAL(7,3), high DECIMAL(7,3), low DECIMAL(7,3), close DECIMAL(7,3), volume INT(9) UNSIGNED, amount MEDIUMINT(12) UNSIGNED)
 
 BEGIN
-	DECLARE clo DECIMAL(6,2);
 	SET `ids` = IF(ids = '', NULL, ids);
 	SET `d` = IF(d = '', NULL, d);
 	SET `open` = IF(open = '', NULL, open);
@@ -84,23 +49,57 @@ BEGIN
 	SET `close` = IF(close = '', NULL, close);
 	SET `volume` = IF(volume = '', NULL, volume);
 	SET `amount` = IF(amount = '', NULL, amount);
-	SET `clo` = IF(close = '', NULL, close);
 
 	IF volume > 0 AND volume IS NOT NULL THEN
-		INSERT INTO `s_xts` (`ids`, `d`, `open`, `high`, `low`, `close`, `volume`, `amount`, `clo`) VALUES (ids, d, open, high, low, close, volume, amount, clo) 
+		INSERT INTO `s_xts` (`ids`, `d`, `open`, `high`, `low`, `close`, `volume`, `amount`) VALUES (ids, d, open, high, low, close, volume, amount) 
 		ON DUPLICATE KEY UPDATE
 			`open` = open,
 			`high` =high,
 			`low` = low,
 			`close` = close,
 			`volume` = volume,
-			`amount` = amount,
-			`clo` = clo;
+			`amount` = amount;
+		INSERT INTO `ying_calc`.`s_xts_adj` (`ids`, `d`, `open`, `high`, `low`, `close`, `volume`, `amount`) VALUES (ids, d, open, high, low, close, volume, amount) 
+		ON DUPLICATE KEY UPDATE
+			`open` = open,
+			`high` =high,
+			`low` = low,
+			`close` = close,
+			`volume` = volume,
+			`amount` = amount;
 	END IF;
 END$$
 DELIMITER ;
 
 
-CALL s_xts('[标签:ids]', '[标签:d]', '[标签:open]', '[标签:high]', '[标签:low]', '[标签:close]', '[标签:volume]', '[标签:amount]') 
+CALL s_xts('[标签:ids]', '[标签:d]', '[标签:open]', '[标签:high]', '[标签:low]', '[标签:close]', '[标签:volume]', '[标签:amount]');
 
+INSERT INTO `s_xts`
+	(`ids`,
+	`d`,
+	`open`,
+	`high`,
+	`low`,
+	`close`,
+	`volume`,
+	`amount`)
+SELECT 
+    `hs_s_xts`.`code`,
+    `hs_s_xts`.`date`,
+    `hs_s_xts`.`open`,
+    `hs_s_xts`.`high`,
+    `hs_s_xts`.`low`,
+    `hs_s_xts`.`close`,
+    `hs_s_xts`.`volume`,
+    `hs_s_xts`.`amount`
+FROM `hs_s_xts` WHERE date = '2015-10-21' ;
 -- 
+SELECT COUNT(*) FROM `hs_s_xts`;
+SELECT COUNT(*) FROM `s_xts`;
+SELECT * FROM `hs_s_xts_fast` ORDER BY date DESC;
+SELECT * FROM `s_xts` WHERE d = '2015-10-22' ORDER BY d DESC;
+
+-- clean data
+SELECT COUNT(*) FROM `s_xts` WHERE `amount` = 0 OR volume = 0;
+SELECT * FROM `s_xts` WHERE `amount` = 0 OR volume = 0;
+DELETE FROM `s_xts` WHERE `amount` = 0 OR volume = 0;
--- query/query_s_xts_adj.sql
+++ query/query_s_xts_adj.sql
@@ -1,10 +1,10 @@
-SELECT * FROM ying_calc.s_xts_adj;
+SELECT * FROM `ying_calc`.`s_xts_adj`;
 
-SELECT count(*) FROM ying_calc.s_xts_adj;
+SELECT count(*) FROM `ying_calc`.`s_xts_adj`;
 
-SELECT max(amount) FROM ying_calc.s_xts_adj;
+SELECT max(amount) FROM `ying_calc`.`s_xts_adj`;
 
-SELECT * FROM ying_calc.s_xts_adj ORDER BY d desc;
+SELECT * FROM `ying_calc`.`s_xts_adj` ORDER BY d desc;
 
 
 
