c9c28f2b13edc328e2b09e2506470dea38e4276f
tedddy <tedddy@foxmail.com>
2015-10-31 23:30:55 +0800

    run

--- proc/sma_TMPL/run_proc_sma_TMPL.sql
+++ proc/sma_TMPL/run_proc_sma_TMPL.sql
@@ -1,12 +1,9 @@
 -- mysql ying_calc -u gxh -p;
 
--- CALL `s_xts_adj_ids_d_close_sma_multiPeriods_loop_dt_ids`('2015-01-01 00:00:01', '2016-01-01 00:00:01', 5, 10, 20, 30, 60, 120); -- run this in putty console
--- 
--- CALL `s_xts_adj_ids_d_amount_sma_multiPeriods_loop_dt_ids`('2015-01-01 00:00:01', '2016-01-01 00:00:01', 5, 10, 20, 30, 60, 120); -- run this in putty console
--- 
--- CALL `s_xts_adj_ids_d_close_sma_multiPeriods_loop_dt_ids`('2014-01-01 00:00:01', '2015-01-01 00:00:01', 5, 10, 20, 30, 60, 120);
--- 
--- CALL `s_xts_adj_ids_d_amount_sma_multiPeriods_loop_dt_ids`('2014-01-01 00:00:01', '2015-01-01 00:00:01', 5, 10, 20, 30, 60, 120);
+CALL `s_xts_adj_ids_d_close_sma_multiPeriods_loop_dt_ids`('2015-10-28 00:00:01', '2016-01-01 00:00:01', 5, 10, 20, 30, 60, 120); -- run this in putty console
+
+CALL `s_xts_adj_ids_d_amount_sma_multiPeriods_loop_dt_ids`('2015-10-28 00:00:01', '2016-01-01 00:00:01', 5, 10, 20, 30, 60, 120); -- run this in putty console
+
 
 CALL `s_xts_adj_ids_d_close_sma_multiPeriods_loop_dt_ids`('2013-01-01 00:00:01', '2014-01-01 00:00:01', 5, 10, 20, 30, 60, 120);
 
@@ -17,7 +14,15 @@ CALL `s_xts_adj_ids_d_close_sma_multiPeriods_loop_dt_ids`('2012-01-01 00:00:01',
 CALL `s_xts_adj_ids_d_amount_sma_multiPeriods_loop_dt_ids`('2012-01-01 00:00:01', '2013-01-01 00:00:01', 5, 10, 20, 30, 60, 120);
 
 
-
 CALL `s_xts_adj_ids_d_close_sma_multiPeriods_loop_dt_ids`('2015-10-24 00:00:01', '2016-01-01 00:00:01', 5, 10, 20, 30, 60, 120); -- run this in putty console
 
 CALL `s_xts_adj_ids_d_amount_sma_multiPeriods_loop_dt_ids`('2015-10-24 00:00:01', '2016-01-01 00:00:01', 5, 10, 20, 30, 60, 120); -- run this in putty console
+
+-- CALL `s_xts_adj_ids_d_close_sma_multiPeriods_loop_dt_ids`('2015-01-01 00:00:01', '2016-01-01 00:00:01', 5, 10, 20, 30, 60, 120); -- run this in putty console
+-- 
+-- CALL `s_xts_adj_ids_d_amount_sma_multiPeriods_loop_dt_ids`('2015-01-01 00:00:01', '2016-01-01 00:00:01', 5, 10, 20, 30, 60, 120); -- run this in putty console
+-- 
+-- CALL `s_xts_adj_ids_d_close_sma_multiPeriods_loop_dt_ids`('2014-01-01 00:00:01', '2015-01-01 00:00:01', 5, 10, 20, 30, 60, 120);
+-- 
+-- CALL `s_xts_adj_ids_d_amount_sma_multiPeriods_loop_dt_ids`('2014-01-01 00:00:01', '2015-01-01 00:00:01', 5, 10, 20, 30, 60, 120);
+
